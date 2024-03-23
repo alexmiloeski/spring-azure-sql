91fc1f443d967f091d7a4adb13416e722a9766d7
alexmiloeski <a.miloeski@gmail.com>
2024-03-23 13:10:33 +0100

    ffs remove import

--- src/test/java/com/example/springwebjdk17/SpringWebJdk17ApplicationTests.java
+++ src/test/java/com/example/springwebjdk17/SpringWebJdk17ApplicationTests.java
@@ -1,7 +1,5 @@
 package com.example.springwebjdk17;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
