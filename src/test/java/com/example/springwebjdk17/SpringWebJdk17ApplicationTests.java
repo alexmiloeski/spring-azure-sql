b1de755b9a1705abcaffcb493e945ba3adb3ed2e
alexmiloeski <a.miloeski@gmail.com>
2024-03-27 20:07:28 +0100

    enable spring test

--- src/test/java/com/example/springwebjdk17/SpringWebJdk17ApplicationTests.java
+++ src/test/java/com/example/springwebjdk17/SpringWebJdk17ApplicationTests.java
@@ -1,38 +1,38 @@
-//package com.example.springwebjdk17;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.web.client.TestRestTemplate;
-//import org.springframework.boot.test.web.server.LocalServerPort;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//class SpringWebJdk17ApplicationTests {
-//
-//	@Autowired
-//	private TestRestTemplate template;
-//
-//	@LocalServerPort
-//	private int port;
-//
-//	@Value("${CUSTOM_VALUE_1}")
-//	private String myCustomValue1;
-//
-//	/*@BeforeAll
-//	public static void foo() {
-//		var dotenv = Dotenv.configure().load();
-//		dotenv.entries().forEach((entry) -> System.setProperty(entry.getKey(), entry.getValue()));
-//	}*/
-//
-//	@Test
-//	public void contextLoads() {
-//		System.out.println("myCustomValue1 = " + myCustomValue1);
-//		String rez = template.getForObject("http://localhost:" + port, String.class);
-//		System.out.println("rez = " + rez);
-//		assertEquals("myCustomValue1=[" + myCustomValue1 + "]", rez);
-//	}
-//
-//}
+package com.example.springwebjdk17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SpringWebJdk17ApplicationTests {
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@LocalServerPort
+	private int port;
+
+	@Value("${CUSTOM_VALUE_1}")
+	private String myCustomValue1;
+
+	/*@BeforeAll
+	public static void foo() {
+		var dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach((entry) -> System.setProperty(entry.getKey(), entry.getValue()));
+	}*/
+
+	@Test
+	public void contextLoads() {
+		System.out.println("myCustomValue1 = " + myCustomValue1);
+		String rez = template.getForObject("http://localhost:" + port, String.class);
+		System.out.println("rez = " + rez);
+		assertEquals("myCustomValue1=[" + myCustomValue1 + "]", rez);
+	}
+
+}
